18fe08ddc69c2be4fc148a2388f50fd0ba9469cb
code-ananya <ananyapandey22222@gmail.com>
2025-10-29 09:03:50 +0530

    Fix:   Correct populate syntax for friends data

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -25,13 +25,13 @@ export async function getMyFriends(req,res) {
     try {
         const user=await User.findById(req.user._id)
         .select("friends")
-        .populate("friends","fullName,profilePic,nativeLanguage,learningLanguage");
+        .populate("friends","fullName profilePic nativeLanguage learningLanguage");
         res.status(200).json(user.friends);
     } catch (error) {
         console.error("Error in getMyFriends controller",error.message);
          res.status(500).json({message:"Internal Server Error"});
 
-        
+
     }
 }
 export async function sendFriendRequest(req,res){
